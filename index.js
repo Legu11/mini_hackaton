c961dda468bc7c320e68c7e8aff594f214571d1c
coubitic <raviola64@povland.com>
2026-03-23 14:42:46 +0100

    base du projet

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// salut
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+
+const app = express();
+
+// connecter la DB
+connectDB();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('aaaaaaaaaAAAAAAAAAAAAAAAAAAAAAAAAH');
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur http://localhost:${PORT}`);
+});
